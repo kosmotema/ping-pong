13c93f451840553d3e3dd8d855b1b433e2c4f098
Anatoly Pitikin <git@xapdkop.dev>
2020-11-22 03:36:41 +0300

    Some more improvements to configs (#5)
    * fix configs and gitignore
    
    * remove ts-node-dev, update server.ts and server's config

--- server.ts
+++ server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify';
 import fastifyStatic from 'fastify-static';
 
-const server = fastify({ logger: false })
-    .register(fastifyStatic, {
-        root: __dirname,
-    }).get('/', async function (_request, reply) {
-        return reply.sendFile('game.html')
-    })
-
-const start = async () => {
-    try {
-        await server.listen(3000);
-    } catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
-}
-
-start();
+(async () => {
+    fastify()
+        .register(fastifyStatic, {
+            root: __dirname,
+        })
+        .get('/', async (_request, reply) => reply.sendFile('game.html'))
+        .listen(3000);
+})();
